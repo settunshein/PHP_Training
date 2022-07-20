5c8ec408b0d27547d3bae726057c2d7ee2d4deb4
Set Tun Shein <setwebdev@gmail.com>
2022-07-20 13:06:37 +0630

    PHP Training Day 03 - Tutorial 02

---
+++ Tutorial_02/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+$row = 6;
+    
+for($i=1; $i<=$row; $i++) {
+    for($j=$row; $j>$i; $j--) {
+        echo "&nbsp;";
+    }
+    for($k=0; $k<$i*2-1; $k++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+
+for($i=1; $i<=$row-1; $i++) {
+    for($j=0; $j<$i; $j++) {
+        echo "&nbsp;";
+    }
+    for($k=($row-$i)*2-1; $k>0; $k--) {
+        echo "*";
+    }
+    echo "<br>";
+}
